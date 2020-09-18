997b47c68e78a0e0a536c8ccc80eddc1a1e82749
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2020-09-18 21:23:05 +0100

    user logi and signup validators added

--- server.js
+++ server.js
@@ -8,13 +8,10 @@ let port=300
 
 
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
 app.use(cors());
 app.use(bodyParser.json());
-
 mongoose.connect('mongodb://127.0.0.1:27017/bugs', { useNewUrlParser: true, useUnifiedTopology:true });
 const connection = mongoose.connection;
-
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
@@ -23,13 +20,13 @@ app.use(express.static('component'));
 app.use(express.static('public'));  
 
 
-// app.use('/todos', todoRoutes)
+
 
 app.get('/', (req, res)=>{
-    res.send("running")
+    res.send(`running on port ${port}`)
 })
 
-
+// app.use('/todos', todoRoutes)
 
 app.listen(port,  (e)=>{
     if(e){
